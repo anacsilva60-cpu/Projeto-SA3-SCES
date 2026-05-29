740e00bfd334816beddb11ad2aea1c674cd028c8
anacsilva60-cpu <ana.c.silva60@edu.senai.br>
2026-05-29 13:43:26 -0300

    Estrutura inicial do SCES.

---
+++ Sistema_de_Estoque.py
@@ -0,0 +1,12 @@
+produtos = [
+    [120, "Prego", 34, ],
+]
+
+def adicionar_produto():
+    int(input("Digite o ID do produto: "))
+    input("Digite o nome do produto: ")
+    int(input("Digite a quantidade de produto que tem no estoque: "))
+    input("Digite o localização do produto: ")
+
+adicionar_produto()
+
