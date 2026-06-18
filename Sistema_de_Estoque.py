d20214b3266de48a5c4c6ae7eb7dbd25ba5378a2
anacsilva60-cpu <ana.c.silva60@edu.senai.br>
2026-06-18 10:11:19 -0300

    Adicionei a função de listar produtos e o menu interativo

--- Sistema_de_Estoque.py
+++ Sistema_de_Estoque.py
@@ -1,12 +1,44 @@
 produtos = [
-    [120, "Prego", 34, ],
+    [100, "Volante", 34, "Setor - 01"],
 ]
 
 def adicionar_produto():
-    int(input("Digite o ID do produto: "))
-    input("Digite o nome do produto: ")
-    int(input("Digite a quantidade de produto que tem no estoque: "))
-    input("Digite o localização do produto: ")
+    global produtos
+    id_item = int(input("Digite o ID do produto: "))
+    nome = input("Digite o nome do produto: ")
+    qtd = int(input("Digite a quantidade de produto que tem no estoque: "))
+    local = input("Digite o localização do produto: ")
+    produtos.append([id_item, nome, qtd, local])
+    travar_menu()
 
-adicionar_produto()
 
+def listar_produtos():
+    for linha in produtos:
+        print(linha)
+    travar_menu()
+
+def buscar_produto():
+    global produtos
+    buscar_id = int(input("Digite o ID do produto para encontrá-lo: "))
+    for produto in produtos:
+        if produto == buscar_id:
+            print("A")
+        else:
+            print("O seu produto não contém no estoque!")
+            print("Adicione um produto para pracurá-lo!!")
+
+buscar_produto()
+
+def travar_menu():
+    input("\nPrecione <ENTER> para continuar... ")
+
+print("\n----> Bem Vindo ao Menu Interativo do Sistema de Estoque! Por Favor, selecione uma opção: ")
+while True: 
+    print("\n1- Mostrar Status do Estoque | 2- Adicionar Produtos no Estoque | 3- Buscar Produto por ID ")
+    opção = input("Escolha: ")
+    if(opção == "1"):
+        listar_produtos()
+    elif(opção == "2"):
+        adicionar_produto()
+    elif(opção == "3"):
+        buscar_produto()
